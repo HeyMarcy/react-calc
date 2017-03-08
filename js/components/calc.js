93de4eba5b9bc9797d3bfff5af44c18a3ac086ce
marcy <marcy.montross@gmail.com>
2017-03-08 12:14:43 -0500

    first commit

---
+++ js/components/calc.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Calc extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputString: '';
+    }
+  }
+  render() {
+    return(
+      
+    )
+  }
+}
--- js/index.js
+++ js/index.js
@@ -1,2 +1,9 @@
 require('babel-polyfill');
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Calc from './calc';
+
+document.addEventListener('DOMContentLoaded', () =>
+  ReactDOM.render(<Calc />, document.getElementByID('app'))
+);
